284fe371e83b043781b040371c1ac5d763862c88
Ross Riley <riley.ross@gmail.com>
2015-07-31 08:54:17 +0100

    psr-fix

--- Extension.php
+++ Extension.php
@@ -7,11 +7,11 @@ use Bolt\BaseExtension;
 
 class Extension extends BaseExtension
 {
-    
     const CONTAINER = 'extensions.formeditor';
-    
+
     /**
-     * Constructor adds an additional Twig path if we are in the Backend
+     * Constructor adds an additional Twig path if we are in the Backend.
+     *
      * @param Application $app
      */
     public function __construct(Application $app)
@@ -25,38 +25,35 @@ class Extension extends BaseExtension
     /**
      * Mounts the editor onto /bolt/extensions/formeditor, and
      * adds the relevant assets.
-     * 
-     * @return void
      */
     public function initialize()
     {
-        $path = $this->app['config']->get('general/branding/path') . '/extensions/formeditor';
-        
+        $path = $this->app['config']->get('general/branding/path').'/extensions/formeditor';
+
         if ($this->checkAuth()) {
             $this->app->mount($path, new Controller\FormEditorController());
             $this->addJavascript('assets/jquery.sortable.min.js', 1);
             $this->addJavascript('assets/formeditor.js', 1);
             $this->addCss('assets/formeditor.css');
-            $this->addMenuOption('Edit Forms', $this->app['resources']->getUrl('bolt') . 'extensions/formeditor', 'fa:pencil-square-o');
+            $this->addMenuOption('Edit Forms', $this->app['resources']->getUrl('bolt').'extensions/formeditor', 'fa:pencil-square-o');
         }
-
     }
-    
+
     /**
      * Checks that the user has a non-guest role.
-     * 
+     *
      * @return bool
      */
     public function checkAuth()
     {
         $currentUser = $this->app['users']->getCurrentUser();
         $currentUserId = $currentUser['id'];
-        foreach (['admin','root','developer', 'editor'] as $role) {
+        foreach (['admin', 'root', 'developer', 'editor'] as $role) {
             if ($this->app['users']->hasRole($currentUserId, $role)) {
                 return true;
             }
         }
-        
+
         return false;
     }
 
