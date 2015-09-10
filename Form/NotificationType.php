b9357735b7ac14ef46d8b5856a19ef7277fc3f34
Ross Riley <riley.ross@gmail.com>
2015-09-10 14:13:16 +0100

    allow config of the from email

--- Form/NotificationType.php
+++ Form/NotificationType.php
@@ -16,6 +16,7 @@ class NotificationType extends AbstractType
             ->add('subject',   'text', [])
             ->add('to_name',   'text', [])
             ->add('to_email',   'text', []);
+            ->add('from_email',  'text', ['This email appears in the From address when sending a notification (as well as an email you can also use a field name)']);
     }
 
     public function getName()
