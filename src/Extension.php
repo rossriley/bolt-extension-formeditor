2f6bf2705a71deb480d5d96e8bd3eaeba8677195
Ross Riley <riley.ross@gmail.com>
2016-06-04 11:43:00 +0100

    add in select2 full version for tag compatibility

--- src/Extension.php
+++ src/Extension.php
@@ -29,6 +29,7 @@ class Extension extends SimpleExtension
     {
         return [
             (new Stylesheet('formeditor.css'))->setZone('backend'),
+            (new JavaScript('select2.full.min.js'))->setZone('backend'),
             (new JavaScript('formeditor.js'))->setZone('backend'),
             (new JavaScript('jquery.sortable.js'))->setZone('backend'),
         ];
