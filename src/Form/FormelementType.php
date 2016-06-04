5aae586d15a12b8b9706f0f6ea2065b3333ba74a
Ross Riley <riley.ross@gmail.com>
2016-06-04 13:16:51 +0100

    force cast nulls to array

--- src/Form/FormelementType.php
+++ src/Form/FormelementType.php
@@ -50,7 +50,7 @@ class FormelementType extends AbstractType
                     'required' => false,
                     'multiple' => true,
                     'attr' => ['help' => 'Setup the available choices'],
-                    'choices' => array_combine($data['choices'],$data['choices']),
+                    'choices' => array_combine((array) $data['choices'], (array) $data['choices']),
                 ]);
             }
         );
